c368df94036cdf65e19a66624124674eabfd1857
Ammar <Ammar@AMMAR-PC>
2017-10-25 15:38:37 -0700

    Add project files.

---
+++ My first program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_first_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helloo World");
+            Console.ReadKey();
+        }
+    }
+}
